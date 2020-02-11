a62174f6fa8c78be256e00b5a9a39eeab6517c93
hodersolutions <47269382+hodersolutions@users.noreply.github.com>
2020-02-11 19:59:12 +0530

    Update models.py

--- logins/models.py
+++ logins/models.py
@@ -342,9 +342,9 @@ class UsersProfileBasic(db.Model):
                 return None
             for userprofile in result:
                 userprofile_dict = dict(zip(result.keys(), userprofile))
-		print(userprofile_dict)
+                print(userprofile_dict)
                 res = UsersProfileBasic.serialize_view_userprofile(userprofile_dict)
-		print(res)
+                print(res)
             return  UsersProfileBasic.serialize_view_userprofile(userprofile_dict)        
         except:
             return None
@@ -565,7 +565,7 @@ class UsersProfileBasic(db.Model):
             "email": userprofile_dict["email"],
             "mobile":userprofile_dict["mobile"]
         }
-    
+        print(json_user)
         return json_user
 
         
